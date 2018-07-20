bcd2a0fea13079492258c478ab42d92be14d8d06
Leyla <leyline@astralfield.blue>
2018-07-20 11:23:13 +0900

    update: 構造をClassに変更

--- lambda_function.py
+++ lambda_function.py
@@ -1,26 +1,47 @@
 import os
 import urllib.request,urllib.parse
-import json
-from datetime import datetime,timedelta
-import datetime
 
-APIEndPoint = "https://slack.com/api/reminders.add"
-legacyToken = os.environ["Token"]
-delayMinute = os.environ["DelayMinute"]
-postChannel = os.environ["PostChannel"]
+class SlackRemind():
+    def __init__(self):
+        self.EndPoint = "https://slack.com/api/reminders.add"
+        self.userToken = os.environ["UserToken"]
+        self.delayMinute = os.environ["DelayMinute"] + " minutes"
+        self.postChannel = os.environ["PostChannel"]
+        self.text="<!here> きっと白湯が生成されました"
+    def CleatePayload(self):
+        payload={"token": self.userToken, "text": self.text, "time": self.delayMinute, "channel": self.postChannel}
+        return payload
+
+class SlackPost():
+    def __init__(self):
+        self.EndPoint = "https://slack.com/api/chat.postMessage"
+        self.botToken = os.environ["BotToken"]
+        self.postChannel = os.environ["PostChannel"]
+        self.text="中身は空でした"
+    def CleatePayload(self):
+        payload={"token": self.botToken, "text": self.text, "channel": self.postChannel}
+        return payload
+        
 
 def lambda_handler(event,context):
-    # リマインダーをセットする時間を計算 
-    remindTime = (datetime.datetime.now()+datetime.timedelta(hours=9,minutes=delayMinute)).strftime("%H:%M")
+    if "deviceEvent" in event:
+        if "buttonClicked" in event["deviceEvent"]:
+            if "clickType" in event["deviceEvent"]["buttonClicked"]:
+                clickType = event["deviceEvent"]["buttonClicked"]["clickType"]
+            else: clickType = "clicktype is none"
+        else: clickType = "buttonClicked is none"
+    else:
+         clickType = "event is none"
+    postClass = SlackRemind()
     # HTTPリクエストするためのパラメータを設定
     method = "POST"
     headers = {"Content-Type" : "application/x-www-form-urlencoded"}
-    payload={"token":legacyToken,"text": "<!here> きっと白湯が生成されました","time":remindTime,"channel":postChannel}
+    payloadResult = postClass.CleatePayload()
     # payloadをURLencode形式に変更
-    p = "?" + urllib.parse.urlencode(payload)
+    p = "?" + urllib.parse.urlencode(payloadResult)
     
     # HTTPリクエストを準備してPOST 
-    request = urllib.request.Request(APIEndPoint+p, method=method, headers=headers)
+    request = urllib.request.Request(postClass.EndPoint+p, method=method, headers=headers)
     with urllib.request.urlopen(request) as response:
         response_body = response.read().decode("utf-8")
         print(response_body)
